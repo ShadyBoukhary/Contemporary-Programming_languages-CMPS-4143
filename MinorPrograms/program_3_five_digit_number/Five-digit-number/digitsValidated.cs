46420d77eb113d8a28b4f34ce16dd85417b1f951
Shady Boukhary <sb199898.sb@gmail.com>
2018-09-03 18:35:36 -0500

    program 3 extra

---
+++ MinorPrograms/program_3_five_digit_number/Five-digit-number/digitsValidated.cs
@@ -0,0 +1,91 @@
+﻿/* Shady Boukhary
+ * CMPS 4143: Contemporary Programming Languages
+ * September 5th, 2018
+ * Dr. Stringfellow
+ * Description: A simple program that reads an integer from the console, 
+ * then displays its digits separately.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiveDigitsValidated
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool run = true;
+        
+            PrintHeader();
+      
+            while (run)
+            {
+                Console.Write("Enter a number: ");
+                int num = ReadInteger();
+
+                foreach(int x in GetDigits(num))
+                {
+                    Console.Write(String.Format("{0}   ", x));
+                }
+
+                Console.Write("\nRun again? (Y/N): ");
+
+                if (Console.ReadLine().ToUpper() == "N")
+                {
+                    run = false;
+                }
+
+            }
+        }
+
+        /// <summary>
+        /// Gets digits in an integer
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns>Array of the digits</returns>
+        public static int[] GetDigits(int num)
+        {
+            int[] digits = new int[num.ToString().Length];
+            for (int x = 0; x < digits.Length; x++)
+            {
+                digits[x] = num % 10;
+                num = num / 10;
+            }
+            Array.Reverse(digits);
+            return digits;
+        }
+
+        /// <summary>
+        /// Reads an integer from the console and validates it.
+        /// </summary>
+        /// <returns>Returns an integer</returns>
+        public static int ReadInteger()
+        {
+            int x;
+            String res = Console.ReadLine();
+
+            while (!Int32.TryParse(res, out x))
+            {
+                Console.WriteLine("Not a valid integer. Try again.");
+                res = Console.ReadLine();
+            }
+            return x;
+        }
+
+        /// <summary>
+        /// Prints information about the program.
+        /// </summary>
+        public static void PrintHeader()
+        {
+            Console.WriteLine("Shady Boukhary\n" +
+                "Dr. StringFellow\n" +
+                "September 5th, 2018\n" +
+                "CMPS 4143: Contemporary Programming Languages\n" +
+                "Welcome to Five Digit Integer Program\n\n");
+        }
+    }
+}
