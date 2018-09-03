0160556990465f4f11e4ccca1f860c82e65e6e86
Shady Boukhary <sb199898.sb@gmail.com>
2018-09-03 17:23:23 -0500

    radio gui

---
+++ MinorPrograms/program_1_radio_gui/Radio/Form1.cs
@@ -0,0 +1,27 @@
+﻿/* Shady Boukhary
+ * CMPS 4143: Contemporary Programming Languages
+ * September 5th, 2018
+ * Dr. Stringfellow
+ * DEscription: GUI-only Radio app
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Radio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
