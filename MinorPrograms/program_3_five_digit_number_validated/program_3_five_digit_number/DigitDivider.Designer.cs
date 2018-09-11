7142199e6b7b056695594cd127073bb905442fc2
Shady Boukhary <sb199898.sb@gmail.com>
2018-09-11 15:11:06 -0500

    program 3 extra credit

---
+++ MinorPrograms/program_3_five_digit_number_validated/program_3_five_digit_number/DigitDivider.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace program_3_five_digit_number_validated
+{
+    partial class DigitDivider
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.promptLabel = new System.Windows.Forms.Label();
+            this.showDigitsButton = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputTextBox.Location = new System.Drawing.Point(135, 58);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(214, 23);
+            this.inputTextBox.TabIndex = 0;
+            this.inputTextBox.TextChanged += new System.EventHandler(this.inputTextBox_TextChanged);
+            // 
+            // promptLabel
+            // 
+            this.promptLabel.AutoSize = true;
+            this.promptLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.promptLabel.Location = new System.Drawing.Point(131, 23);
+            this.promptLabel.Name = "promptLabel";
+            this.promptLabel.Size = new System.Drawing.Size(218, 21);
+            this.promptLabel.TabIndex = 1;
+            this.promptLabel.Text = "Please enter a 5-digit number.";
+            // 
+            // showDigitsButton
+            // 
+            this.showDigitsButton.BackColor = System.Drawing.Color.DodgerBlue;
+            this.showDigitsButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.showDigitsButton.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.showDigitsButton.ForeColor = System.Drawing.Color.White;
+            this.showDigitsButton.Location = new System.Drawing.Point(135, 96);
+            this.showDigitsButton.Name = "showDigitsButton";
+            this.showDigitsButton.Size = new System.Drawing.Size(214, 37);
+            this.showDigitsButton.TabIndex = 2;
+            this.showDigitsButton.Text = "Show Digits";
+            this.showDigitsButton.UseVisualStyleBackColor = false;
+            this.showDigitsButton.Click += new System.EventHandler(this.showDigitsButton_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultLabel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.resultLabel.Location = new System.Drawing.Point(12, 151);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(459, 40);
+            this.resultLabel.TabIndex = 3;
+            this.resultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // DigitDivider
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(483, 220);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.showDigitsButton);
+            this.Controls.Add(this.promptLabel);
+            this.Controls.Add(this.inputTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "DigitDivider";
+            this.Text = "Digit Divider";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Label promptLabel;
+        private System.Windows.Forms.Button showDigitsButton;
+        private System.Windows.Forms.Label resultLabel;
+    }
+}
+
---
+++ MinorPrograms/program_3_five_digit_number_validated/program_3_five_digit_number/DigitDivider.cs
@@ -0,0 +1,74 @@
+﻿/* Shady Boukhary
+ * CMPS 4143: Contemporary Programming Languages
+ * Program 3 Extra Credit
+ * September 12th, 2018
+ * Dr. Stringfellow
+ * Description: GUI App - The user enters an integer then its digits are displayed seperately.
+*/
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace program_3_five_digit_number_validated
+{
+    public partial class DigitDivider : Form
+    {
+        public DigitDivider()
+        {
+            InitializeComponent();
+            showDigitsButton.Enabled = false;
+        }
+
+        /* Event Handlers */
+
+        private void showDigitsButton_Click(object sender, EventArgs e)
+        {
+            int num;
+
+            if (Int32.TryParse(inputTextBox.Text, out num) && inputTextBox.Text.Length < 6)
+            {
+                resultLabel.ForeColor = Color.Black;
+                resultLabel.Font = new Font("Segoe UI", 21, FontStyle.Bold);
+                resultLabel.Text = "";
+                foreach(int x in GetDigits(num))
+                {
+                    resultLabel.Text += String.Format("{0}   ", x);
+                }
+            } else
+            {
+                resultLabel.ForeColor = Color.Red;
+                resultLabel.Font = new Font("Segoe UI", 12, FontStyle.Bold);
+                resultLabel.Text = "Input must be a 5-digit-or-less number. Try again.";
+            }
+        }
+
+        private void inputTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (inputTextBox.Text == "")
+            {
+                showDigitsButton.Enabled = false;
+                return;
+            }
+
+            showDigitsButton.Enabled = true;
+        }
+
+        /// <summary>
+        /// Gets digits in an integer
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns>Array of the digits</returns>
+        public static int[] GetDigits(int num)
+        {
+            int[] digits = new int[num.ToString().Length];
+            for (int x = 0; x < digits.Length; x++)
+            {
+                digits[x] = num % 10;
+                num = num / 10;
+            }
+            Array.Reverse(digits);
+            return digits;
+        }
+    }
+}
