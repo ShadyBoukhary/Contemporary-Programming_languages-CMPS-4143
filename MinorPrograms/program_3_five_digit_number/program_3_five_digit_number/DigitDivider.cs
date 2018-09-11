56c319bcc7e517b47cc7bb819e8f60c37ee41833
Shady Boukhary <sb199898.sb@gmail.com>
2018-09-11 14:43:38 -0500

    changeg to gui app

---
+++ MinorPrograms/program_3_five_digit_number/program_3_five_digit_number/DigitDivider.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace program_3_five_digit_number
+{
+    public partial class DigitDivider : Form
+    {
+        public DigitDivider()
+        {
+            InitializeComponent();
+            showDigitsButton.Enabled = false;
+        }
+
+        private void showDigitsButton_Click(object sender, EventArgs e)
+        {
+            int num;
+
+            if (Int32.TryParse(inputTextBox.Text, out num))
+            {
+                resultLabel.ForeColor = Color.Black;
+                resultLabel.Font = new Font("Segoe UI", 21, FontStyle.Bold);
+                resultLabel.Text = String.Format("{0}   {1}   {2}   {3}   {4}\n",
+                   GetDigit(num, 10000), GetDigit(num, 1000), GetDigit(num, 100), GetDigit(num, 10), num % 10);
+            } else
+            {
+                resultLabel.ForeColor = Color.Red;
+                resultLabel.Font = new Font("Segoe UI", 12, FontStyle.Bold);
+                resultLabel.Text = "You did not enter a number. Try again.";
+            }
+        }
+
+        private void inputTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (inputTextBox.Text == "")
+            {
+                showDigitsButton.Enabled = false;
+                return;
+            }
+
+            showDigitsButton.Enabled = true;
+        }
+
+        /// <summary>
+        /// Finds a digit in an integer
+        /// </summary>
+        /// <param name="num"></param>
+        /// <param name="div"></param>
+        /// <returns></returns>
+        public static int GetDigit(int num, int div)
+        {
+            return num / div % 10;
+        }
+
+        private void resultLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MinorPrograms/program_3_five_digit_number/program_3_five_digit_number/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("program_3_five_digit_number")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("program_3_five_digit_number")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9623f2e3-2f03-4f8f-905f-2e348ba0940d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
